3d4bdaf215165de32d01b782e2868eead6b0ea56
ángela <al_amartinezn@iessierradelasvillas.org>
2022-10-05 21:41:16 +0200

    modificación reto spiderman

--- reto spiderman.py
+++ reto spiderman.py
@@ -1,8 +1,8 @@
 caminoA = 0
 caminoB = 0 
 
-caminoA=(int)(input("¿En qué punto es la primera misión(punto más cercano a 0)?"))
-caminoB=(int)(input("¿En qué punto es la segunda misión(punto más lejano a 0?"))
+caminoA=(int)(input("¿En qué punto es la primera misión(punto más cercano a 0? "))
+caminoB=(int)(input("¿En qué punto es la segunda misión(punto más lejano a 0? "))
 
 if(caminoA<0 and caminoB<0):
     print("La distancia recorrida es: ",abs(caminoB),)
@@ -11,4 +11,4 @@ if(caminoA>0 and caminoB>0):
 if(caminoA<0 and caminoB>0):
     print("La distancia recorrida es: ",abs(caminoA)*2+caminoB,)
 if(caminoA>0 and caminoB<0):
-    print("La distancia recorrida es: ",abs(caminoB)*2+caminoA,)
+    print("La distancia recorrida es: ",caminoA*2+abs(caminoB),)
