f3711ea0e5ff63023d317c19571cc3b7529f6562
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-10-10 11:15:27 +0200

    Agenda telefonos

---
+++ agenda tlfs.py
@@ -0,0 +1,23 @@
+'''
+Opcion 1:
+Lista para nombres
+Lista para telefonos
+
+Opcion 2:
+Lista para nombres [Juan-telefono, Pepe-telefno]
+
+'''
+
+#Opcion 1:
+vNombres = []
+vTelefonos = []
+
+
+nombres=input("Dime un nombre ")
+telefonos=input ("Dime tu telefono")
+
+vNombres.append(nombres)
+vTelefonos.append(telefonos)
+
+print(vNombres)
+print(vTelefonos)
