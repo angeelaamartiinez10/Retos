c0e3f26c2f996176dd695a69461a531d1a01cc15
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-10-17 11:15:58 +0200

    menú en la agenda

--- agenda tlfs.py
+++ agenda tlfs.py
@@ -12,12 +12,31 @@ Lista para nombres [Juan-telefono, Pepe-telefno]
 vNombres = []
 vTelefonos = []
 
+#Creamos el menu de la agenda
+'''
+1-Insertar contacto
+2-Borrar contacto
+3-Buscar contacto
+4-Ver todos los contactos
+5-Salir
+'''
+opcion=0
 
-nombres=input("Dime un nombre ")
-telefonos=input ("Dime tu telefono ")
+while (opcion!=5):
+    print("Pulsa 1 para INSERTAR un contacto")
+    print("Pulsa 2 para BORRAR un contacto")
+    print("Pulsa 3 para BUSCAR un contacto")
+    print("Pulsa 4 para VER todos los contacto")
+    print("Pulsa 5 si desea SALIR de su agenda")
+    opcion=(int)(input("SELECCIONE UNA OPCION: "))
+    try:
+        opcion=(int)(input("SELECCIONE UNA OPCION: "))
+    except:
+        print("Las opciones son del 1 al 5")
 
-vNombres.append(nombres)
-vTelefonos.append(telefonos)
+
+
+
 
 
 #El telefono de juan es: ejercicio casa
