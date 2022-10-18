6fbba4138a6c56f6de37df68ee4cb78f39a56963
Ángela Martínez <al_amartinezn@iessierradelasvillas.org>
2022-10-18 14:32:52 +0200

    agenda editada con funciones

--- agenda tlfs.py
+++ agenda tlfs.py
@@ -20,19 +20,29 @@ vTelefonos = []
 4-Ver todos los contactos
 5-Salir
 '''
-opcion=0
-
-while (opcion!=5):
-    print("Pulsa 1 para INSERTAR un contacto")
-    print("Pulsa 2 para BORRAR un contacto")
-    print("Pulsa 3 para BUSCAR un contacto")
-    print("Pulsa 4 para VER todos los contacto")
-    print("Pulsa 5 si desea SALIR de su agenda")
-    opcion=(int)(input("SELECCIONE UNA OPCION: "))
-    try:
+#funcion que pinta el menu y devuelve la opcion seleccionada del 1 al 5
+def pintaMenu():
+    opcion=0
+    while (opcion<1 or opcion>5):
+        print("*****AGENDA*****")
+        print("Pulsa 1 para INSERTAR un contacto")
+        print("Pulsa 2 para BORRAR un contacto")
+        print("Pulsa 3 para BUSCAR un contacto")
+        print("Pulsa 4 para VER todos los contacto")
+        print("Pulsa 5 si desea SALIR de su agenda")
+        print("************")
         opcion=(int)(input("SELECCIONE UNA OPCION: "))
-    except:
-        print("Las opciones son del 1 al 5")
+        try:
+            opcion=(int)(input("SELECCIONE UNA OPCION: "))  #si da error lo que hay dentro del try salta
+        except:
+            print("Las opciones son del 1 al 5")
+    
+    return opcion
+
+
+opcion=pintaMenu()
+while(opcion!=5):
+    opcion=pintaMenu()
 
 
 
